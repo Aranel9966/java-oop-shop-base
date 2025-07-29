b37afef482fdefc0b32edada0d90b34c0e0c8080
Aranel9966 <99angelo.dire@gmail.com>
2025-07-29 18:58:44 +0200

    aggiunti medoti get e set

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,33 +3,67 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    int codice;
-    String nome;
-    String descrizione;
-    double prezzo;
-    int iva;
-    // consturttore con parametro 
-    Prodotto(int codice, String nome, String descrizione, double prezzo, int iva){
-        this.codice= codice;
-        this.descrizione=descrizione;
-        this.nome= nome;
-        this.prezzo=prezzo;
-        this.iva=iva;
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private int iva;
 
-        
+    // consturttore con parametro
+    Prodotto(int codice, String nome, String descrizione, double prezzo, int iva) {
+        this.codice = codice;
+        this.descrizione = descrizione;
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.iva = iva;
+
     };
 
-    public void printPrezzo(){
-        System.out.println("prezzo: "+ prezzo + " euro");
+    public String getNome() {
+        return this.nome;
     }
 
-    public void printCodName(){
-        System.out.println("codice competo prodotto: " + codice+"-"+nome);
+    public String getDescrizione() {
+        return this.descrizione;
     }
 
-    public void printPrezzoIva(){
-        double prezzoIva = prezzo * (1 + iva / 100);
-        System.out.println("prezzo compreso di iva: " + prezzoIva + " euro");
+    public int getCodice() {
+        return this.codice;
     }
+
+    public String getPrezzo() {
+        String prezzoCompeto = "prezzo: " + prezzo + " euro";
+        return prezzoCompeto;
+    }
+
+    public String getCodName() {
+        String codName = "codice competo prodotto: " + codice + "-" + nome;
+        return codName;
+    }
+
+    public String getPrezzoIva() {
+        String prezzoIva = "prezzo compreso di iva: " + (((prezzo * iva) / 100) + prezzo) + " euro";
+        return prezzoIva;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
 }
-
--- src/org/lessons/java/shop/main.java
+++ src/org/lessons/java/shop/main.java
@@ -5,15 +5,26 @@ import java.util.Random;
 public class main {
 
     public static void main(String[] args) {
-        int id= 0;
+        int id = 0;
         Random randomNumber = new Random();
         id = randomNumber.nextInt(50);
-        Prodotto scarpa = new Prodotto(id, "jordan", "scarpe belle", 70, 20);
-        
-        // System.out.println("prezzo:"+ scarpa.prezzo + " euro");
-        scarpa.printPrezzo();
-        scarpa.printCodName();
-        scarpa.printPrezzoIva();;
-        
+        Prodotto scarpa = new Prodotto(id, "Jordan", "scarpe belle", 70, 20);
+
+        System.out.println(scarpa.getNome());
+        System.out.println(scarpa.getDescrizione());
+        System.out.println(scarpa.getCodice());
+        System.out.println(scarpa.getCodName());
+        System.out.println(scarpa.getPrezzo());
+        System.out.println(scarpa.getPrezzoIva());
+
+        scarpa.setNome("Super");
+        System.out.println(scarpa.getNome());
+        scarpa.setDescrizione("scarpe burtte");
+        System.out.println(scarpa.getDescrizione());
+        scarpa.setPrezzo(120.32);
+        System.out.println(scarpa.getPrezzo());
+        scarpa.setIva(30);
+        System.out.println(scarpa.getIva());
+
     }
 }
